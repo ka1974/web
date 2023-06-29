1e1125592d5f4de18024d557ad03f7896c8ab01a
ka1974 <kabashimaxxx@gmail.com>
2023-06-29 13:15:24 +0900

    Create page1.vue

---
+++ page1.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ title }}</h1>
+  <ph-sun :size="60" />
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      title: 'page1.vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
