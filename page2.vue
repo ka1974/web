f8f6ec2a6a99f896a7eef7424fbd34bf0215f32d
ka1974 <kabashimaxxx@gmail.com>
2023-06-29 13:15:27 +0900

    Create page2.vue

---
+++ page2.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ title }}</h1>
+  <ph-umbrella :size="60" />
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      title: 'page2.vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: blue;
+}
+</style>
